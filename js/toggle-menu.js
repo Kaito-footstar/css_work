e176c1f877e797ceada22ef59042a4844194fe19
Kaito-footstar <251205121@ccmailg.meijo-u.ac.jp>
2026-06-15 15:47:50 +0900

    import files

---
+++ js/toggle-menu.js
@@ -0,0 +1,22 @@
+window.addEventListener('load', function () {
+    var $button = document.querySelector('.toggle-menu-bunnton');
+    var $menu = document.querySelector('.header-siet-menu');
+    $button.addEventListener('click', function () {
+        if ($menu.classList.contains('is-show')) {
+            $menu.classList.remove('is-show');
+        }
+        else {
+            $menu.classList.add('is-show');
+
+        }
+        
+    });
+});
+
+$(function () {
+    $(window).scroll(function() {
+        $("nav.floating").stop().animate (
+            {"top": $(window).scrollTop() + 100},
+        500);
+    });
+});
